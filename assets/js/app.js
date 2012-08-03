4ab24621ef36b7a40a10120b31bfdec2b9f95d76
davids5 <david_s5@nscdg.com>
2012-08-03 11:14:39 -1000

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,7 @@ run(function () {
             alert("No internet connection - we won't be able to track your time");
         } else {
 		try {
-                        window.plugins.childBrowser.openExternal("http://myttpro.com",1);        
+                        window.plugins.childBrowser.openExternal("http://myttpro.com",0);        
                     }
                     catch (err)
                     {
