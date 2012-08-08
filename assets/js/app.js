267709a59a0c50bb72448e148160493f2544dc0a
davids5 <david_s5@nscdg.com>
2012-08-08 09:01:57 -1000

    wip

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,6 @@
 //  --- our app behavior logic ---
 //
 
-function initListener() {
-    
-        if (typeof window.attachEvent != 'undefined') {
-	    top.attachEvent('onblur', processIFrameClick);
-        }
-	else if (typeof window.addEventListener != 'undefined') {
-	 top.addEventListener('blur', processIFrameClick, false);
-      }
-  }
 run(function () {
     // immediately invoked on first run
     var init = (function () {
@@ -18,8 +9,7 @@ run(function () {
             alert("No internet connection - we won't be able to track your time");
         } else {
 		try {
-			initListener();
-			openExternal('http://myttpro.com',{ showLocationBar: false, showNavigationBar: false, showAddress: false});     
+			openExternal('http://www.myttpro.com',{ showLocationBar: false, showNavigationBar: false, showAddress: false});     
                     }
                     catch (err)
                     {
