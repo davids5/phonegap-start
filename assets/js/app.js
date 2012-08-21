4e834481903d837cdc2106435100b88de3ee8369
davids5 <david_s5@nscdg.com>
2012-08-21 12:19:34 -1000

    forst run

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,7 +9,13 @@ run(function () {
             alert("No internet connection - we won't be able to track your time");
         } else {
 		try {
-			openExternal('http://myttpro.com/frame.html',{ showLocationBar: false, showNavigationBar: false, showAddress: false});     
+			window.localStorage.removeItem("first_run");
+			if (window.localStorage.getItem("first_run") == null) {
+			    openExternal('http://myttpro.com/frame.html?m=SignUp',{ showLocationBar: false, showNavigationBar: false, showAddress: false});
+			} else {
+			    openExternal('http://myttpro.com/frame.html',{ showLocationBar: false, showNavigationBar: false, showAddress: false});
+			}
+			window.localStorage.setItem("first_run","true");
                     }
                     catch (err)
                     {
